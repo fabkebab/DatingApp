5edb51d7c03ecbf8788ea2bf9e8ea03a158a916c
andrew <fabkebab@gmail.com>
2018-08-04 09:53:29 -0500

    Initial Commit

---
+++ DatingApp.API/Controllers/FunController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+
+         ///http://localhost:5000/api/Fun
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FunController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value3" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            //throw new Exception ("bad news - it didnt work!");
+            return "value was " + id + " and its a FUN controller so deal with it!";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    
+}
---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.DataModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    ///http://localhost:5000/api/Values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private DataContext _Context ;
+        public ValuesController(DataContext context){
+            this._Context = context  ;     
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult>  GetValues()
+        {
+            //return new string[] { "value1", "value3" };
+            var values = await _Context.Values.ToListAsync() ;
+            return Ok(values) ;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            //throw new Exception ("bad news - it didnt work!");
+            var values = await _Context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            // ToList() ;
+            
+            //return Ok("value was " + values.Name + " so deal with it!");
+            return Ok(values);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
